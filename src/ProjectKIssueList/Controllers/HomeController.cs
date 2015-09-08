bc8ab35aae6b530bf5eaffdbd4c160a6e339401a
Eilon Lipton <elipton@microsoft.com>
2015-09-07 22:28:32 -0700

    Fix small rendering bug in PR list

---
+++ src/ProjectKIssueList/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ProjectKIssueList.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/ProjectKIssueList/Controllers/IssueListController.cs
+++ src/ProjectKIssueList/Controllers/IssueListController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Http.Authentication;
 using Microsoft.AspNet.Mvc;
+using Microsoft.Framework.Internal;
 using Octokit;
 using Octokit.Internal;
 using ProjectKIssueList.Models;
@@ -38,29 +39,42 @@ namespace ProjectKIssueList.Controllers
             return gitHubClient.PullRequest.GetAllForRepository("aspnet", repo);
         }
 
-        [Route("{repoSet?}")]
-        public IActionResult Index(string repoSet)
+        public class GitHubAuthDataAttribute : ActionFilterAttribute
         {
-            var gitHubAccessToken = Context.Session.GetString("GitHubAccessToken");
-            var gitHubName = Context.Session.GetString("GitHubName");
-
-            // If session state didn't have our data, either there's no one logged in, or they just logged in
-            // but the claims haven't yet been read.
-            if (string.IsNullOrEmpty(gitHubAccessToken))
+            public override void OnActionExecuting(ActionExecutingContext context)
             {
-                if (!User.Identity.IsAuthenticated)
+                var gitHubAccessToken = context.HttpContext.Session.GetString("GitHubAccessToken");
+                var gitHubName = context.HttpContext.Session.GetString("GitHubName");
+
+                // If session state didn't have our data, either there's no one logged in, or they just logged in
+                // but the claims haven't yet been read.
+                if (string.IsNullOrEmpty(gitHubAccessToken))
                 {
-                    // Not authenticated at all? Go to GitHub to authorize the app
-                    return new ChallengeResult("GitHub", new AuthenticationProperties { RedirectUri = "/" + repoSet });
+                    if (!context.HttpContext.User.Identity.IsAuthenticated)
+                    {
+                        // Not authenticated at all? Go to GitHub to authorize the app
+                        context.Result = new ChallengeResult(
+                            authenticationScheme: "GitHub",
+                            properties: new AuthenticationProperties { RedirectUri = "/" });
+                        return;
+                    }
+
+                    // Authenticated but haven't read the claims? Process the claims
+                    gitHubAccessToken = context.HttpContext.User.FindFirst("access_token")?.Value;
+                    gitHubName = context.HttpContext.User.Identity.Name;
+                    context.HttpContext.Session.SetString("GitHubAccessToken", gitHubAccessToken);
+                    context.HttpContext.Session.SetString("GitHubName", gitHubName);
                 }
 
-                // Authenticated but haven't read the claims? Process the claims
-                gitHubAccessToken = Context.User.FindFirst("access_token")?.Value;
-                gitHubName = Context.User.Identity.Name;
-                Context.Session.SetString("GitHubAccessToken", gitHubAccessToken);
-                Context.Session.SetString("GitHubName", gitHubName);
+                context.ActionArguments.Add("gitHubAccessToken", gitHubAccessToken);
+                context.ActionArguments.Add("gitHubName", gitHubName);
             }
+        }
 
+        [Route("{repoSet}")]
+        [GitHubAuthData]
+        public IActionResult Index(string repoSet, string gitHubAccessToken, string gitHubName)
+        {
             // Authenticated and all claims have been read
 
             var repos =

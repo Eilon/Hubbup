d833c21ab9d50bfe267905a637060ebef554e3d2
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2017-07-05 01:12:10 +0000

    Webpack, React and Preview2, oh my! (#102)
    * webpack, react and preview2, oh my!
    
    * converted templating to use react
    * use webpack for bundling and module management
    * slight css restyling
    * update to 2.0.0-preview2-final
    
    * pr feedback: consistent quoting
    
    * pr feedback: const all the things
    
    * pr feedback: strict (in)equality
    
    * pr feedback: remove extraneous type
    
    * pr feedback: remove unnecessary return
    
    * pr feedback: resp.ok and try-catch
    
    * pr feedback: consistent braces
    
    * pr feedback: idiomatic setState and more try-catch
    
    * pr feedback: more resp.ok
    
    * pr feedback: remove unnecessary async
    
    * pr feedback: remove extraneous webpack code
    
    * pr feedback: remove extraneous launchSettings.json
    
    * fix bug in json data loading

---
+++ src/Hubbup.Web/webpack.config.js
@@ -0,0 +1,62 @@
+"use strict";
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const context = path.join(__dirname, 'Client');
+const wwwroot = path.join(__dirname, 'wwwroot');
+const dist = path.join(wwwroot, 'dist');
+
+module.exports = {
+    entry: {
+        global: "./global.ts",
+        standup: "./Standup/page.tsx"
+    },
+    context: context,
+    devtool: 'inline-source-map',
+    output: {
+        filename: "[name].js",
+        path: dist,
+    },
+    devServer: {
+        contentBase: ".",
+        host: "localhost",
+        port: 9000,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader',
+                }),
+            },
+            {
+                test: /\.(otf|eot|svg|ttf|woff2?)$/,
+                loader: 'file-loader?name=[name].[hash].[ext]'
+            }
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(dist),
+        new ExtractTextPlugin('[name].css'),
+
+        // Borrowed from https://webpack.js.org/guides/code-splitting-libraries/#implicit-common-vendor-chunk
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        })
+    ],
+};

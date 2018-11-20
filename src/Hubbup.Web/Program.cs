21576445ac8fdc8729845e2a83f1b1ba20e07311
Chris Ross (ASP.NET) <chrross@microsoft.com>
2018-11-19 16:02:43 -0800

    Move IDataSource initialization to Main

--- src/Hubbup.Web/Program.cs
+++ src/Hubbup.Web/Program.cs
@@ -1,13 +1,26 @@
+using System.Threading.Tasks;
+using Hubbup.Web.DataSources;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Hubbup.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            // Load data before we start things up.
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogDebug("Loading repo set and person set data...");
+            var dataSource = host.Services.GetRequiredService<IDataSource>();
+            await dataSource.ReloadAsync(default);
+            logger.LogDebug("Loaded repo set and person set data");
+
+            await host.RunAsync();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>

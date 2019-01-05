2c469ade015f35cf266e1efd0f719e65485bc419
MikaelMengistu <ethmengistu7@gmail.com>
2019-01-04 16:03:21 -0800

    Issue Labeler First Commit

--- src/Hubbup.Web/Controllers/MikLabelerController.cs
+++ src/Hubbup.Web/Controllers/MikLabelerController.cs
@@ -2,12 +2,17 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Hubbup.Web.DataSources;
 using Hubbup.Web.Utils;
+using Hubbup.Web.ML;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Octokit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
 
 namespace Hubbup.Web.Controllers
 {
@@ -17,13 +22,21 @@ namespace Hubbup.Web.Controllers
     {
         private readonly IDataSource _dataSource;
         private readonly ILogger<MikLabelerController> _logger;
+        private static string AppPath => Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+        private static string ModelPath = "/ML/GitHubLabelerModel.zip";
+        private static string ModelFilePathName = $"GitHubLabelerModel.zip";
+        public enum MyTrainerStrategy : int { SdcaMultiClassTrainer = 1, OVAAveragedPerceptronTrainer = 2 };
+        public static IConfiguration Configuration { get; set; }
+        private readonly IHostingEnvironment _hostingEnvironment;
 
         public MikLabelerController(
             IDataSource dataSource,
-            ILogger<MikLabelerController> logger)
+            ILogger<MikLabelerController> logger,
+            IHostingEnvironment hostingEnvironment)
         {
             _dataSource = dataSource;
             _logger = logger;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         [Route("")]
@@ -31,7 +44,71 @@ namespace Hubbup.Web.Controllers
         {
             var accessToken = await HttpContext.GetTokenAsync("access_token");
             var gitHub = GitHubUtils.GetGitHubClient(accessToken);
-            return View(await gitHub.Issue.GetAllForRepository("aspnet", "AspNetCore", new ApiOptions { PageSize = 10, PageCount = 1 }));
+
+            //This line re-trains the ML Model
+            //MLHelper.BuildAndTrainModel(_hostingEnvironment.ContentRootPath + "/ML/issueData.tsv", _hostingEnvironment.ContentRootPath + ModelFilePathName, MyTrainerStrategy.OVAAveragedPerceptronTrainer);
+
+            //2. Try/test to predict a label for a single hard-coded Issue
+            var labeler = new Labeler(_hostingEnvironment.ContentRootPath + ModelPath);
+
+            var issues = await gitHub.Issue.GetAllForRepository("aspnet", "AspNetCore", new ApiOptions { PageSize = 100, PageCount = 1 });
+
+            var issueList = new List<Issue>();
+            var predictionLabels = new List<GitHubIssuePrediction>();
+            foreach (Issue issue in issues)
+            {
+                if (issue.PullRequest != null)
+                {
+                    continue;
+                }
+
+                var areaLabel = findAreaLabel(issue);
+                if (areaLabel == null)
+                {
+                    issueList.Add(issue);
+                    var predictedLabel = labeler.PredictLabel(issue);
+                    predictionLabels.Add(predictedLabel);
+                }
+            }
+
+            ViewData["Predictions"] = predictionLabels;
+            return View(issueList);
+        }
+
+        [HttpPost]
+        [Route("ApplyLabel")]
+        public async Task<IActionResult> ApplyLabel(int issueNumber, string prediction)
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var gitHub = GitHubUtils.GetGitHubClient(accessToken);
+            var issue = await gitHub.Issue.Get("aspnet", "aspnetcore", issueNumber);
+            ApplyPredictedLabel(issue, prediction, gitHub);
+            return RedirectToAction("Index");
+        }
+
+        private void ApplyPredictedLabel(Issue issue, string label, IGitHubClient client)
+        {
+            var issueUpdate = new IssueUpdate();
+            issueUpdate.AddLabel(label);
+
+            client.Issue.Update("aspnet", "aspnetcore", issue.Number, issueUpdate);
+
+            Console.WriteLine($"Issue {issue.Number} : \"{issue.Title}\" \t was labeled as: {label}");
+        }
+
+        private static Label findAreaLabel(Issue issue)
+        {
+            var labels = issue.Labels;
+            foreach (Label label in labels)
+            {
+                if (label.Name.StartsWith("area-"))
+                {
+                    return label;
+                }
+            }
+
+            //No area label
+            return null;
         }
     }
 }
---
+++ src/Hubbup.Web/ML/Labeler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ML.Core.Data;
+using Microsoft.ML;
+using Octokit;
+using System.IO;
+using Microsoft.ML.Runtime.Data;
+
+namespace Hubbup.Web.ML
+{
+    //This "Labeler" class could be used in a different End-User application (Web app, other console app, desktop app, etc.)
+    public class Labeler
+    {
+        private readonly string _modelPath;
+        private readonly MLContext _mlContext;
+        private readonly PredictionFunction<GitHubIssue, GitHubIssuePrediction> _predFunction;
+        private readonly ITransformer _trainedModel;
+
+        public Labeler(string modelPath)
+        {
+            _modelPath = modelPath;
+            _mlContext = new MLContext(seed:1);
+
+            //Load model from file
+            
+            using (var stream = new FileStream(_modelPath, System.IO.FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                _trainedModel = _mlContext.Model.Load(stream);
+            }
+
+            // Create prediction engine related to the loaded trained model
+            _predFunction = _trainedModel.MakePredictionFunction<GitHubIssue, GitHubIssuePrediction>(_mlContext);
+        }
+
+        public GitHubIssuePrediction PredictLabel(Octokit.Issue issue)
+        {
+            var aspnetIssue = new GitHubIssue
+            {
+                ID = issue.Number.ToString(),
+                Title = issue.Title,
+                Description = issue.Body
+            };
+
+            var predictedLabel = Predict(aspnetIssue);
+
+            return predictedLabel;
+        }
+
+        public GitHubIssuePrediction Predict(GitHubIssue issue)
+        {          
+            return _predFunction.Predict(issue);
+        }
+    }
+}

5e813bea5d9b2fc7ebc0a0a69480ef3f1f3e6a34
Eilon Lipton <Eilon@users.noreply.github.com>
2019-07-30 10:08:31 -0700

    Update more NuGet dependencies

--- src/Hubbup.IssueMoverClient/AppState.cs
+++ src/Hubbup.IssueMoverClient/AppState.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using Hubbup.IssueMover.Dto;
 using Hubbup.IssueMoverApi;
 using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 
 namespace Hubbup.IssueMoverClient
 {

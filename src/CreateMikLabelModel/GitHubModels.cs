3a8d4ab54beb935c6737d4e2db8e6ccd327ad4a5
Eilon Lipton <Eilon@users.noreply.github.com>
2020-06-01 21:08:09 -0700

    Use GitHub GraphQL API for MikLabeler ML training
    - Switch from GitHub REST API to GraphQL API
    - This leads to an improvement from ~610sec to ~471sec to download all the issue data (23% faster)

---
+++ src/CreateMikLabelModel/GitHubModels.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+// Various models used to deserialize GraphQL responses from GitHub
+namespace CreateMikLabelModel
+{
+    public class GitHubIssueListPage
+    {
+        public bool IsError { get; set; }
+        public Data Issues { get; set; }
+    }
+
+    public class Data
+    {
+        public Repository Repository { get; set; }
+    }
+
+    public class Repository
+    {
+        public string Name { get; set; }
+        public Issues Issues { get; set; }
+    }
+
+    public class Issues
+    {
+        public List<IssuesNode> Nodes { get; set; }
+        public PageInfo PageInfo { get; set; }
+        public long TotalCount { get; set; }
+    }
+
+    public class IssuesNode
+    {
+        public long Number { get; set; }
+        public string Title { get; set; }
+        public string BodyText { get; set; }
+        public Labels Labels { get; set; }
+    }
+
+    public class Labels
+    {
+        public List<LabelsNode> Nodes { get; set; }
+        public long TotalCount { get; set; }
+    }
+
+    public class LabelsNode
+    {
+        public string Name { get; set; }
+    }
+
+    public class PageInfo
+    {
+        public bool HasNextPage { get; set; }
+        public string EndCursor { get; set; }
+    }
+}
--- src/CreateMikLabelModel/Program.cs
+++ src/CreateMikLabelModel/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
 using Hubbup.MikLabelModel;
 using Microsoft.Extensions.Configuration;
-using Octokit;
 
 namespace CreateMikLabelModel
 {
@@ -19,14 +19,31 @@ namespace CreateMikLabelModel
             ("dotnet", "extensions"),
         };
 
-        static async Task Main()
+        static async Task<int> Main()
         {
             foreach (var repo in Repos)
             {
                 var tsvRawGitHubDataPath = $"{repo.owner}-{repo.repo}-issueData.tsv";
                 var modelOutputDataPath = $"{repo.owner}-{repo.repo}-GitHubLabelerModel.zip";
 
-                await GetGitHubIssueData(repo.owner, repo.repo, outputPath: tsvRawGitHubDataPath);
+                var stopWatch = Stopwatch.StartNew();
+
+                using (var outputWriter = new StreamWriter(tsvRawGitHubDataPath))
+                {
+                    WriteCsvHeader(outputWriter);
+
+                    if (!await ProcessGitHubIssueData(repo.owner, repo.repo, IssueType.Issue, outputWriter))
+                    {
+                        return -1;
+                    }
+                    if (!await ProcessGitHubIssueData(repo.owner, repo.repo, IssueType.PullRequest, outputWriter))
+                    {
+                        return -1;
+                    }
+                }
+
+                stopWatch.Stop();
+                Console.WriteLine($"Done writing TSV in {stopWatch.ElapsedMilliseconds}ms");
 
                 //This line re-trains the ML Model
                 MLHelper.BuildAndTrainModel(
@@ -39,80 +56,185 @@ namespace CreateMikLabelModel
             }
 
             Console.WriteLine($"Please remember to copy the ZIP files to the web site's ML folder");
+
+            return 0;
         }
 
-        private static async Task GetGitHubIssueData(string owner, string repo, string outputPath)
+        private static async Task<bool> ProcessGitHubIssueData(string owner, string repo, IssueType issueType, StreamWriter outputWriter)
         {
-            Console.WriteLine($"Getting all issues for {owner}/{repo}...");
+            Console.WriteLine($"Getting all '{issueType}' items for {owner}/{repo}...");
 
-            var stopWatch = Stopwatch.StartNew();
-
-            // Get credentials
-            var ghc = GitHubClientFactory.Create() as GitHubClient;
-
-            var allIssues = new List<(string owner, string repo, Issue issue)>();
-
-            var allIssuesInRepo = await ghc.Issue.GetAllForRepository(
-                owner,
-                repo,
-                new RepositoryIssueRequest
+            using (var ghGraphQL = CreateGraphQLClient())
+            {
+                bool hasNextPage;
+                string afterID = null;
+                var totalProcessed = 0;
+                do
                 {
-                    State = ItemStateFilter.All,
-                    SortProperty = IssueSort.Created,
-                    SortDirection = SortDirection.Descending,
+                    var issuePage = await GetGitHubIssuePage(ghGraphQL, owner, repo, issueType, afterID);
+
+                    if (issuePage.IsError)
+                    {
+                        Console.WriteLine("Error encountered in GraphQL query. Stopping.");
+                        return false;
+                    }
+
+                    var issuesOfInterest =
+                        issuePage.Issues.Repository.Issues.Nodes
+                            .Where(i => IsIssueOfInterest(i))
+                            .ToList();
+
+                    var uninterestingIssuesWithTooManyLabels =
+                        issuePage.Issues.Repository.Issues.Nodes
+                            .Except(issuesOfInterest)
+                            .Where(i => i.Labels.TotalCount > 10);
+
+                    if (uninterestingIssuesWithTooManyLabels.Any())
+                    {
+                        // The GraphQL query gets at most 10 labels per issue. So if an issue has more than 10 labels,
+                        // but none of the first 10 are an 'area-' label, then it's possible that one of the unseen
+                        // labels is an 'area-' label and we don't know about it. So we warn.
+                        foreach (var issue in uninterestingIssuesWithTooManyLabels)
+                        {
+                            Console.WriteLine(
+                                $"\tWARNING: Issue {owner}/{repo}#{issue.Number} has more than 10 labels " +
+                                $"and the first 10 aren't 'area-' labels so it is ignored.");
+                        }
+                    }
+
+                    totalProcessed += issuePage.Issues.Repository.Issues.Nodes.Count;
+                    Console.WriteLine(
+                        $"Processing {totalProcessed}/{issuePage.Issues.Repository.Issues.TotalCount}. " +
+                        $"Writing {issuesOfInterest.Count} items of interest to output TSV file...");
+
+                    foreach (var issue in issuesOfInterest)
+                    {
+                        WriteCsvIssue(outputWriter, issue);
+                    }
+                    hasNextPage = issuePage.Issues.Repository.Issues.PageInfo.HasNextPage;
+                    afterID = issuePage.Issues.Repository.Issues.PageInfo.EndCursor;
+                }
+                while (hasNextPage);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns 'true' if the issue has at least one 'area-*' label, meaning it can be
+        /// used to create training data.
+        /// </summary>
+        /// <param name="issue"></param>
+        /// <returns></returns>
+        private static bool IsIssueOfInterest(IssuesNode issue)
+        {
+            return issue.Labels.Nodes.Any(l => l.Name.StartsWith("area-", StringComparison.OrdinalIgnoreCase));
+        }
+
+        private static void WriteCsvHeader(StreamWriter outputWriter)
+        {
+            outputWriter.WriteLine("ID\tArea\tTitle\tDescription");
+        }
+
+        private static void WriteCsvIssue(StreamWriter outputWriter, IssuesNode issue)
+        {
+            var area = issue.Labels.Nodes.First(l => l.Name.StartsWith("area-", StringComparison.OrdinalIgnoreCase)).Name;
+            var body = issue.BodyText.Replace('\r', ' ').Replace('\n', ' ').Replace('\t', ' ');
+            outputWriter.WriteLine($"{issue.Number}\t{area}\t{issue.Title}\t{body}");
+        }
+
+        private enum IssueType
+        {
+            Issue,
+            PullRequest,
+        }
+
+        private static async Task<GitHubIssueListPage> GetGitHubIssuePage(GraphQLHttpClient ghGraphQL, string owner, string repo, IssueType issueType, string afterID)
+        {
+            var issueNodeName = issueType switch
+            {
+                IssueType.Issue => "issues", // Query for issues
+                IssueType.PullRequest => "issues:pullRequests", // Query for pull requests, but rename the node to 'issues' to re-use code
+                _ => throw new ArgumentOutOfRangeException(nameof(issueType)),
+            };
+
+            var issueRequest = new GraphQLRequest(
+                query: @"query ($owner: String!, $name: String!, $afterIssue: String) {
+  repository(owner: $owner, name: $name) {
+    name
+    " + issueNodeName + @"(after: $afterIssue, first: 100, orderBy: {field: CREATED_AT, direction: DESC}) {
+      nodes {
+        number
+        title
+        bodyText
+        labels(first: 10) {
+          nodes {
+            name
+          },
+          totalCount
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      totalCount
+    }
+  }
+}
+",
+                variables: new
+                {
+                    owner = owner,
+                    name = repo,
+                    afterIssue = afterID,
                 });
 
-            Console.WriteLine($"Found {allIssuesInRepo.Count} total issues");
-
-            var issuesOfInterest =
-                allIssuesInRepo
-                    .Where(i => i.Labels.Any(l => l.Name.StartsWith("area-", StringComparison.OrdinalIgnoreCase)))
-                    .ToList();
-
-            Console.WriteLine($"Found {issuesOfInterest.Count} of interest (have at least 1 area label)");
-
-            Console.WriteLine($"Writing to output TSV file {outputPath}...");
-
-            using (var outputWriter = new StreamWriter(outputPath))
+            var result = await ghGraphQL.SendQueryAsync<Data>(issueRequest);
+            if (result.Errors?.Any() ?? false)
             {
-                outputWriter.WriteLine("ID\tArea\tTitle\tDescription");
-                foreach (var issue in issuesOfInterest)
+                Console.WriteLine($"GraphQL errors! ({result.Errors.Length})");
+                foreach (var error in result.Errors)
                 {
-                    var area = issue.Labels.First(l => l.Name.StartsWith("area-", StringComparison.OrdinalIgnoreCase)).Name;
-                    var body = issue.Body.Replace('\r', ' ').Replace('\n', ' ').Replace('\t', ' ');
-                    outputWriter.WriteLine($"{issue.Number}\t{area}\t{issue.Title}\t{body}");
+                    Console.WriteLine($"\t{error.Message}");
                 }
+                return new GitHubIssueListPage { IsError = true, };
             }
 
-            stopWatch.Stop();
-            Console.WriteLine($"Done writing TSV in {stopWatch.ElapsedMilliseconds}ms");
+            var issueList = new GitHubIssueListPage
+            {
+                Issues = result.Data,
+            };
+
+            return issueList;
         }
 
-        private static class GitHubClientFactory
+        private static GraphQLHttpClient CreateGraphQLClient()
         {
-            private static readonly string Version = typeof(GitHubClientFactory).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            var gitHubAccessToken = GetGitHubAuthToken();
 
-            public static IGitHubClient Create()
+            var graphQLClient = new GraphQLHttpClient("https://api.github.com/graphql", new NewtonsoftJsonSerializer());
+            graphQLClient.HttpClient.DefaultRequestHeaders.Authorization =
+                new System.Net.Http.Headers.AuthenticationHeaderValue(
+                    scheme: "bearer",
+                    parameter: gitHubAccessToken);
+            return graphQLClient;
+        }
+
+        private static string GetGitHubAuthToken()
+        {
+            const string UserSecretKey = "GitHubAccessToken";
+
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets("AspNetHello.App")
+                .Build();
+
+            var gitHubAccessToken = config[UserSecretKey];
+            if (string.IsNullOrEmpty(gitHubAccessToken))
             {
-                const string UserSecretKey = "GitHubAccessToken";
-
-                var config = new ConfigurationBuilder()
-                    .AddUserSecrets("AspNetHello.App")
-                    .Build();
-                var gitHubAccessToken = config[UserSecretKey];
-                if (string.IsNullOrEmpty(gitHubAccessToken))
-                {
-                    throw new InvalidOperationException($"Couldn't find User Secret named '{UserSecretKey}' in configuration.");
-                }
-
-                var connection = new Connection(new ProductHeaderValue("AspNetHello.GitHubUtils", Version))
-                {
-                    Credentials = new Credentials(gitHubAccessToken)
-                };
-                var ghc = new GitHubClient(connection);
-
-                return ghc;
+                throw new InvalidOperationException($"Couldn't find User Secret named '{UserSecretKey}' in configuration.");
             }
+            return gitHubAccessToken;
         }
     }
 }

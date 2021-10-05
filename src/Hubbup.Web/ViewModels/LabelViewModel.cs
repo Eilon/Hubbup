43e8146eea3acb5f7de09d332105bfd387905e3f
Eilon Lipton <Eilon@users.noreply.github.com>
2021-10-05 10:56:27 -0700

    Show existing labels on issues in prediction list
    Also update label styling to have rounded corners like on GitHub

---
+++ src/Hubbup.Web/ViewModels/LabelViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Octokit;
+
+namespace Hubbup.Web.ViewModels
+{
+    public class LabelViewModel
+    {
+        public LabelViewModel(Label label, string desiredLabel)
+        {
+            Label = label;
+            DesiredLabel = desiredLabel;
+        }
+
+        public Label Label { get; }
+        public string DesiredLabel { get; }
+    }
+}
